939935e2cd6a4ca918b341e505daf11f3659a733
chris-eminence <peterabasiedimo@gmail,com>
2024-04-03 21:45:15 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:weather_mate/screens/city_screen.dart';
+import 'package:weather_mate/screens/loading_screen.dart';
+import 'package:weather_mate/screens/location_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
